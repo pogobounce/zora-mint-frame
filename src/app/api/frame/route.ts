b46d8fca02e943917e1e62bd7d18b5380af35024
pogo <borgster04@yahoo.com>
2024-02-06 10:35:34 +0200

    updates

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -76,7 +76,7 @@ export async function POST(req: NextRequest): Promise<Response> {
       address: CONTRACT_ADDRESS,
       abi: Zora1155ABI,
       functionName: 'transfer',
-      args: [address, 1000000000000000000n],
+      args: [address, 1000000000000000000],
       account: privateKeyToAccount(MINTER_PRIVATE_KEY),
     });
 
