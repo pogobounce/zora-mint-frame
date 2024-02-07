0f552edc856fb6ad91b5df747e8ab2cece741b7d
pogo <borgster04@yahoo.com>
2024-02-07 23:13:44 +0200

    update

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const CHAIN = base;
 export const CONTRACT_ADDRESS = '0xbd5e4ca04dcdeb296f855bf06135c484c43e17f4';
 
 export const FRAME_METADATA = getFrameMetadata({
-  buttons: ['mint'],
+  buttons: ['claim fingerprint'],
   image: `${SITE_URL}/opengraph-image.jpg`,
   post_url: `${SITE_URL}/api/frame`,
 });
