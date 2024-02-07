d5daf3ec5514c09b44c0916e6531c43298063be6
pogo <borgster04@yahoo.com>
2024-02-07 22:55:37 +0200

    changes

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 export const SITE_URL = 'https://zora-mint-frame-fawn.vercel.app';
 
 export const CHAIN = base;
-export const CONTRACT_ADDRESS = '0xf2daa2c8e2a48b490dcd6a4cde09b60cb0426873';
+export const CONTRACT_ADDRESS = '0xbd5e4ca04dcdeb296f855bf06135c484c43e17f4';
 
 export const FRAME_METADATA = getFrameMetadata({
   buttons: ['mint'],
