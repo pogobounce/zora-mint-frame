ef5d126a842993e5e455a48915d6350a64a18a57
pogo <borgster04@yahoo.com>
2024-02-09 11:21:53 +0200

    end of life update

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,6 @@ export const CONTRACT_ADDRESS = '0xEC7F8de00e20EC9E4180aE1F9F9eaf6250b8A445';
 
 export const FRAME_METADATA = getFrameMetadata({
   buttons: ['ALL MINTS HAVE BEEN CLAIMED'],
-  image: `${SITE_URL}/out_of_mints.jpg`,
+  image: `${SITE_URL}/opengraph-image.jpg`,
   post_url: `${SITE_URL}/api/frame`,
 });
